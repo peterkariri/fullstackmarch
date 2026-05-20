0b58de0b5d88b910b739a511ff39a3257ee71e37
spectre <peternjugunaw25@gmail.com>
2026-05-20 11:55:52 +0300

    final

--- javascript/function1.js
+++ javascript/function1.js
@@ -58,6 +58,19 @@ let data=[
 },
 
 ]
+
+//accesing using string interpolation
+console.log(`My name is ${data[0].name} 
+and I am ${data[0].age}.My gender is ${data[0].gender} where I attend 
+${data[0].institute} to persue a career in ${data[0].course} 
+cohort ${data[0].year} whic has always been my dream. `);
+
+//method
+/* let console={
+    log:function(){
+
+    }
+} */
 console.log(data[2].name);
 
 //sntax of functions 
@@ -69,7 +82,41 @@ arrow function  */
 
 //function declaration
 /* declare the function keyword then the function name then the noraml brackets o gold the parameters 
-afterwards dclare the curly brakcets to list the logic of the function  */
-function getStudents(param1,param2){
-    //logic
+afterwards dclare the curly brakcets to list the logic of the function 
+functions use a return keyword to give back a value after computation and this keyword also terminates the function 
+>>anything after the return keyword is not exectued 
+ when you are executing a function you need to call it (invoking its name using normal brackets at the end of thename)
+ when calling the function you have to pass in arguments in the brackets 
+*/
+function getSum(param1,param2){
+     return param1 * param2
 }
+
+ console.log(getSum(1066666667777,15123456789876543));
+  
+
+ /* theyre are three ways of claring strings {
+    using single quotes 
+    using double quotes 
+    using backtick 
+ }
+
+ we use delimeters with string + concatenation 
+ */
+let firstName="Basil"
+
+let secondName='Juma'
+//using the plus method
+
+let fullname= "My NAME IS "+firstName +" "+ secondName+ ' '+"AND " + " i AM A FULLSTACK SOFTWARE eng"
+
+console.log(fullname);
+
+//usong the temporal strings (backticks )
+/* we use the dollar sign to pass placeholders for variables and data types 
+the dollar sign will have a curly brackets 
+${firstName}
+ */
+let fname=`My Name is ${firstName} ${secondName} and I am a Software Engineering student`
+
+console.log(fname);
