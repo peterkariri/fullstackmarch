742eb91faac17020b2624fc7c77bb6119c6198a4
spectre <peternjugunaw25@gmail.com>
2026-05-28 12:31:58 +0300

    lastone

--- javascript/operators.js
+++ javascript/operators.js
@@ -112,7 +112,138 @@ console.log(mult);
   }
   With logical gates we use them for software eng ,hardware eng 
   */
-//conditonal statements
+//conditonal statements>>are statemtns that conrol the logical execution of code 
+
+//if>>exectues only when a condition is set to truth {
+/* the syntax is as follow>>always use the three equal signs ,the normal brakcets,and the curly brackets 
+
+if(contition){
+  exectuion if the conditionis true 
+  this only executes the truthy part 
+}
+} */
+
+/* let day='Wednesday'
+
+if(day==="Thursday"){
+    console.log("its a Thursday");
+    
+} */
   
+//we introduce the else block to handle the negative part 
 
 
+/* if(day==="Thursday"){
+    console.log("its a Thursday");
+    
+}
+  else{
+    console.log("its another day");
+    
+  } */
+
+ //nested if else >>is used to execute nested logic (authentication ,fetching info from backed 
+ //where comparison is needed ) 
+
+/*  we can have multiple else if to handle various conditonal variations 
+ we can have the else to terminate the condition once a default value is needed after 
+ none of the conditions are met 
+
+ syntax {
+    if(){
+
+    }
+    else if(){
+
+    }
+    ... various else if 
+
+    else(){
+
+    }
+ }
+
+ */
+
+ let marks =78
+
+ if(marks >= 60 && marks <=69){
+    console.log("student has a B-");
+    
+ }
+else if(marks >= 70 && marks <=79){
+    console.log('student has a B');
+    
+}
+else if(marks >= 80 && marks <=89){
+    console.log('student has a B+');
+    
+}
+else if(marks >= 90 && marks <=95){
+    console.log('student has a A-');
+    
+}
+else if(marks >= 96 && marks <=100){
+    console.log('student has a A');
+    
+}
+else{
+    console.log('Invalid Passmark');
+    
+}
+
+//is the switch statement that does almost the same thing 
+/* with the switch statement we use case syntax and break syntax to terminal the loop because if we dont 
+js will execute infinitly  */
+/* define a variable 
+define the switch block 
+inside the switch block you define the cases
+the case >>handles the various cases 
+the break>>avoids and infinite loop 
+the default>gives the default value once no case is satisfied
+*/
+let day='WEDNESDAY'
+
+switch (day) {
+    case 'Monday':
+        console.log('It is monday');
+        
+        break;
+
+        case 'Tuesday':
+        console.log('It is Tuesday');
+        
+        break;
+     case 'Wednesday':
+        console.log('It is Wednesday');
+        
+        break;
+    case 'Thursday':
+        console.log('It is Thursday');
+        
+        break;
+
+        case 'Friday':
+        console.log('It is Friday');
+        
+        break;
+
+    default:
+        console.log('It is a weekend');
+        
+        break;
+}
+
+//BANK 
+
+let balance=50000
+let withdrawal=20000
+
+if(balance>=withdrawal){
+    console.log("Succefful transaction");
+    
+}
+else{
+    console.log("insufficient Funds your Balance is "+ balance );
+    
+}
