550e5bf9b70f1a3ef92888a147e06785254570b2
spectre <peternjugunaw25@gmail.com>
2026-05-28 10:49:38 +0300

    neew

---
+++ javascript/operators.js
@@ -0,0 +1,85 @@
+//what is an operator {
+//operators are logical controllers that perform a particular action to operands 
+
+/* let sum=9+3
+9,3>>operands
++operator 
+
+
+}
+//arithmetic operators
+
+additional operator{
+    used for aritheetic adition 
+} */
+
+    let sum=9+3;
+
+    console.log(sum);
+    
+
+ //minus/subtraction operator>>does a substraction 
+ 
+ let num1=10
+ let num2=12
+
+ let diff=num2-num1
+
+ console.log(diff);
+
+ //division>whch does a division to numbers >>we use the front slash 
+
+ let div=num2/num1
+
+ console.log(div);
+ 
+ //there is a differece between divisin(/) and modulus (%) the modulus is used to give the reminder of a division 
+
+let mod=num2%num1
+
+console.log(mod);
+
+//multiplication >>we use the asteric Symbol to define multiplying elements (*)
+
+let mult=num2*num1
+
+console.log(mult);
+
+ //comparison operators >>they are used to compare the logic state of various values 
+ //they include >,<,>=,<=,==,====,!==
+
+ let age=18
+
+ if(age>=18){//true
+    console.log("you are a citizen ");
+    
+ }
+ else{//false
+    console.log("you are a minor");
+    
+ }
+
+ //assignment operator>>we assing waluves on the rigt side to what is on te left side 
+ //these operstor is defined by the equal sign 
+
+  let Number=18//18 has been assigned to NUmber so when you want to access it you call number 
+
+  //as compared to the equal sign operator which most tend to confuse 
+
+  //== (equal )
+
+  //=== (explicitly equal )
+
+  let number=50
+
+  let newNumber="50"
+  
+
+  if (number===newNumber){
+    console.log("yes");
+    
+  }
+  else{
+    console.log("change data type explicitly ");
+    
+  }
